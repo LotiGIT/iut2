55fdc0c98ea48ecf8df0bb5b31c6d3e12a962943
ejanot <ejanot@pce-test>
2024-09-11 17:28:48 +0200

    exo 2 tp1

--- R3.01_DeveloppementWeb/TP/TP1/liste.php
+++ R3.01_DeveloppementWeb/TP/TP1/liste.php
@@ -1,4 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice  2</title>
+</head>
+<body>
+    <style>
+        table, th, td{
+            border : 2px;
+        }
+
+        th, td{
+            padding-right : 10px;
+            padding-left : 10px;
+        }
+    </style>
+    <?php
+    $data = file_get_contents('data');
+    $products = unserialize($data);
+    
+    echo "<table><th>Code</th><th>Libellé</th><th>Prix HT</th><th>Taux TVA</th><th>Montant TVA</th><th>Prix TTC</th><th>Stock</th><th>Quantité vendue</th></table>";
+
+    foreach($data as $line){
+        
+    }
+    ?>
     
 
-?>
+
+</body>
+</html>
