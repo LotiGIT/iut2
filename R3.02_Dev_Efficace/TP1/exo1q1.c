7b0afb283d728aa6e18727998959ed78949fca78
ejanot <ejanot@pce-test>
2024-09-11 14:20:18 +0200

    dev efficace tp 1

---
+++ R3.02_Dev_Efficace/TP1/exo1q1.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    int x = 100;
+    itn y = 30;
+    p=(int*)
+    // definition d'un pointeur
+    int *pt *pt2;
+    // affectation de la valeur de x au pointeur pt 
+    pt = &x;
+    y = *pt;
+    *pt = 50;
+    pt2 = pt;
+
+    //*pt = manière d'appeler le pointeur
+    printf("\nla valeur est de : %d\n\n", *pt);
+
+
+    
+    
+}
---
+++ R3.02_Dev_Efficace/TP1/exo2.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef char chaine20[21];
+
+typedef struct{
+    chaine20 nom;
+    int population;
+    chaine20 capitale;
+} t_region;
+
+int main(){
+
+    
+
+}
