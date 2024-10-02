043b4d399617f8faeca15a830b156156c8d62a0b
ejanot <ejanot@pce-test>
2024-10-02 15:49:13 +0200

    fin exo1 q10*

--- R3.02_Dev_Efficace/TP2/exo1.c
+++ R3.02_Dev_Efficace/TP2/exo1.c
@@ -4,6 +4,7 @@
 
 typedef char nom50[50];
 
+// Question 1
 typedef struct{
     int age;
     nom50 nom;
@@ -14,12 +15,14 @@ typedef struct elem{
     struct elem* next;
 }telement;
 
+// Question 2
 typedef telement *tliste;
 
 void init_liste(tliste *l){
     *l = NULL;
 }
 
+// Question 3
 void insertion_en_tete(tliste *l, nom50 nom, int age){
     telement *e = (telement*)malloc(sizeof(telement));
     strcpy(e->personne.nom, nom);
@@ -31,6 +34,7 @@ void insertion_en_tete(tliste *l, nom50 nom, int age){
 
 }
 
+// Question 4
 void affichagePers(tpersonnes p){
     printf("\nNom : %s\nAge : %d ans\n", p.nom, p.age);
 }
@@ -45,7 +49,107 @@ void affichageListe(tliste l){
     }
 }
 
+// Question 5
+telement * existe(tliste l, nom50 leNom){
+    telement * res = NULL;
+    telement *parcours;
+    parcours = l;
 
+    while((res==NULL)&&(parcours!=NULL)){
+        if(strcmp(parcours->personne.nom, leNom)==0){
+            res = parcours;
+        }
+        return res;
+    }
+    return 0;
+
+
+}
+
+// Question 6
+void supprimer_tete(tliste*l){
+    telement * p;
+
+    if(*l!=NULL){
+        p=*l;
+        *l=(*l)->next;
+
+        free(p);
+    }
+}
+
+// Question 7
+int compter(tliste l, nom50 nom){
+    int res = 0;
+    telement *clui;
+    clui = existe(l, nom);
+
+
+    while(clui!=NULL){
+        res++;
+        clui = existe(clui->next, nom);
+    }
+    return res;
+
+}
+
+// Question 8
+telement * recherche(tliste l, int i){
+    int count = l;
+    telement *parcours;
+    parcours = l;
+    
+    while((parcours!=NULL) && (count<i)){
+        count++;
+        parcours = parcours->next;
+    }
+    return parcours;
+
+
+}
+
+// Question 9 
+void insererFinListe(tliste *liste, chaine n, int a){
+    telement*e = (telement*)malloc(sizeof(telement));
+    telement *parcours;
+    strcpy(e->personne.nom, n);
+    e->personne.age = a;
+    e->next = NULL;
+
+    if((*liste)==NULL){
+        *liste= e;
+    }else{
+        parcours = *liste;
+        while(parcours->next !=NULL){
+            parcours = parcours->next;
+        }
+        parcours->next=e;
+    }
+}
+
+// Question 10
+void insererNimporteOu(tliste, chain n, int a, int I){
+    telement* e;
+    telement* parcours;
+
+    int cpt = 1;
+    parcours = liste;
+    while((parcours!=NULL) && (cpt != I)){
+        parcours = parcours -> next;
+        cpt++;
+    }
+
+    if(parcours != NULL){
+        e = (telement*)malloc(sizeof(telement));
+        strcpy(e->pers.nom, n);
+        e->pers.age = a;
+        e->next=parcours->next;
+        parcours->next=e;
+    }else{
+        printf("Pas de place pour insérer l'élémentà la position %d\n", I);
+    }
+
+}
 
 int main(){
     tliste maliste;
@@ -67,6 +171,13 @@ int main(){
         scanf("%s", nom);
     }
     affichageListe(maliste);
+
+    if(existe(maliste, "Michel")==NULL) printf("\nMichel est absent\n");
+    if(existe(maliste, "Maurice")==NULL) printf("\nMaurice est absent\n");
+
+    printf("Nombre de Maurice : %d\n", compter(maliste, "Maurice"));
+
+
 
 }
 
